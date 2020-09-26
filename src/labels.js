96c48e6a9d0edab5528381ef0413b16447bb0891
Igor Cemim <igor@cemim.com.br>
2020-09-26 01:24:59 -0300

    Melhorias no output dos exemplos

--- src/labels.js
+++ src/labels.js
@@ -8,21 +8,34 @@ async function quickstart() {
   // Criação de uma instância do client
   const client = new vision.ImageAnnotatorClient();
 
+  const images = [
+    './resources/cat1.jpg',
+    './resources/cat2.jpg',
+    './resources/cat3.jpg',
+    './resources/dog1.jpg',
+    './resources/dog2.jpg',
+    './resources/dog3.jpg',
+  ];
+
   // Caminho da imagem
-  const imageLocation = './resources/cat1.jpg'
+  for (const imageLocation of images) {
 
-  // O caminho da imagem pode ser uma URL também (não recomendado para ambiente de produção). Exemplo:
-  // const imageLocation = 'https://http.cat/207'
+    // O caminho da imagem pode ser uma URL também (não recomendado para ambiente de produção). Exemplo:
+    // const imageLocation = 'https://http.cat/207'
 
-  // Realiza o reconhecimento de labels na imagem
-  const [result] = await client.labelDetection(imageLocation);
+    // Realiza o reconhecimento de labels na imagem
+    const [result] = await client.labelDetection(imageLocation);
 
-  // Exibe no console os labels para a imagem
-  const labels = result.labelAnnotations;
-  console.log('Labels:');
-  labels.forEach(label => {
-    console.log(label.description);
-  });
+    // Exibe no console os labels para a imagem
+    const labels = result.labelAnnotations;
+    console.log('-------------------------')
+    console.log('Imagem: ' + imageLocation);
+    console.log("\nLabels:");
+    labels.forEach(label => {
+      console.log('-> Descrição => ' + label.description)
+      console.log('   Score => ' + label.score + "\n")
+    });
+  }
 }
 
 quickstart();
--- src/text.js
+++ src/text.js
@@ -7,23 +7,33 @@ async function quickstart() {
 
   // Criação de uma instância do client
   const client = new vision.ImageAnnotatorClient();
+  const images = [
+    './resources/placa1.jpeg',
+    './resources/placa2.jpeg',
+    './resources/placa3.jpeg',
+  ];
 
-  // Caminho da imagem
-  const imageLocation = './resources/placa1.jpeg';
+  for (const img of images) {
 
-  // O caminho da imagem pode ser uma URL também (não recomendado para ambiente de produção). Exemplo:
-  // const imageLocation = 'https://http.cat/207'
+    // Caminho da imagem
+    const imageLocation = './resources/placa3.jpeg';
 
-  // Realiza o reconhecimento de texto na imagem
-  const [result] = await client.textDetection(imageLocation);
-  const [firstDetection] = result.textAnnotations;
-  const detections = result.textAnnotations;
+    // O caminho da imagem pode ser uma URL também (não recomendado para ambiente de produção). Exemplo:
+    // const imageLocation = 'https://http.cat/207'
 
-  // Exibe no console as ocorrências encontradas na imagem
-  console.log('Primeira ocorrência:');
-  console.log(firstDetection.description)
-  console.log('Todas as ocorrências:');
-  detections.forEach(text => console.log(text));
+    // Realiza o reconhecimento de texto na imagem
+    const [result] = await client.textDetection(img);
+    const [firstDetection] = result.textAnnotations;
+    const detections = result.textAnnotations;
+
+    // Exibe no console as ocorrências encontradas na imagem
+    console.log('-------------------------')
+    console.log('Imagem: ' + img);
+    console.log("\nPrimeira ocorrência:");
+    console.log('-> ' + firstDetection.description)
+    console.log("\nTodas as ocorrências:");
+    detections.forEach(text => console.log('-> ' + text.description));
+  }
 }
 
 quickstart();
